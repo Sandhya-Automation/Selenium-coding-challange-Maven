6183fe09df26abe0c44ce5bfff8d6b65159d1380
Sandhya-Automation <sandhya.vanga@gmail.com>
2024-05-29 10:53:06 +0100

    Updated filter handling

--- src/test/java/FilterHandling_ecommerce.java
+++ src/test/java/FilterHandling_ecommerce.java
@@ -13,7 +13,7 @@ public class FilterHandling_ecommerce {
         driver.get("https://www.t-mobile.com/commerce/tablets");
         driver.manage().window().maximize();
         JavascriptExecutor js = (JavascriptExecutor)driver;
-        js.executeScript("window.scrollBy(0,300)", new Object[0]);
+        js.executeScript("window.scrollBy(0,300)");
         selectFilter("Deals", "New");
         selectFilter("Brands", "Apple", "Samsung");
         selectFilter("Operating system", "Android");
@@ -22,7 +22,7 @@ public class FilterHandling_ecommerce {
     private static void selectFilter(String... lists) throws InterruptedException {
         for(int i = 1; i < lists.length; ++i) {
             //System.out.println(lists[i]);
-            Thread.sleep(2000L);
+            Thread.sleep(2000);
             System.out.println(driver.findElement(By.xpath("//span[text()='" + lists[i] + "']")).getText());
             driver.findElement(By.xpath("//span[text()='" + lists[i] + "']")).click();
         }
